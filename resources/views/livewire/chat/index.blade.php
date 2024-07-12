8def9d901d3dcabebf799fc0e3a3f3c8dd5025ae
Nguyen Hoang Nam <nhnam010@gmail.com>
2024-07-12 15:23:10 +0700

    feat: add chat list

--- resources/views/livewire/chat/index.blade.php
+++ resources/views/livewire/chat/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
 
-        {{-- <livewire:chat.chat-list> --}}
+        <livewire:chat.chat-list>
     </div>
 
     <div class="hidden md:grid   w-full border-l h-full relative overflow-y-auto" style="contain:content">
